b2d6144a719ac1f3fefadf53e1ba411b97a22b5d
phu phan <>
2023-11-04 04:32:55 +0700

    first commit

---
+++ lib/widgets/main_draw.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MainDraw extends StatelessWidget {
+  const MainDraw({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: [
+          DrawerHeader(
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Theme.of(context).colorScheme.primaryContainer,
+                  Theme.of(context)
+                      .colorScheme
+                      .primaryContainer
+                      .withOpacity(0.8),
+                ],
+              ),
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.fastfood,
+                  size: 48,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                const SizedBox(width: 18),
+                Text(
+                  'Cooking up!',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleLarge!
+                      .copyWith(color: Theme.of(context).colorScheme.primary),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
